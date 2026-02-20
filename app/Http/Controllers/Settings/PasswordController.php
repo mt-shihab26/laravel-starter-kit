a361c02921e99ce352586192ab3d0842b17810a8
Shihab Mahamud <shihab4t@gmail.com>
2026-02-20 19:36:47 +0600

    auth service provider

--- app/Http/Controllers/Settings/PasswordController.php
+++ app/Http/Controllers/Settings/PasswordController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Settings;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Settings\PasswordUpdateRequest;
 use Illuminate\Http\RedirectResponse;
-use Inertia\Inertia;
 use Inertia\Response;
 
 class PasswordController extends Controller
@@ -15,7 +14,7 @@ class PasswordController extends Controller
      */
     public function edit(): Response
     {
-        return Inertia::render('settings/password');
+        return inertia('settings/password');
     }
 
     /**
