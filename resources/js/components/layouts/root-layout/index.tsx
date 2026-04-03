e18e55707c8a5e9bfa73dd9d67eb42b595d832ba
Shihab Mahamud <shihab4t@gmail.com>
2026-04-04 00:22:07 +0600

    install the shadcn ui components

--- resources/js/components/layouts/root-layout/index.tsx
+++ resources/js/components/layouts/root-layout/index.tsx
@@ -3,14 +3,15 @@ import type { ReactNode } from 'react';
 import { useFlashMessages } from '@/hooks/use-flash-messages';
 
 import { Toaster } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
 
 export const RootLayout = ({ children }: { children: ReactNode }) => {
     useFlashMessages();
 
     return (
-        <>
+        <TooltipProvider>
             {children}
             <Toaster position="top-center" />
-        </>
+        </TooltipProvider>
     );
 };
